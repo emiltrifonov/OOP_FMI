4337107c3b59efcc099fdd3e8478bae42a151cfa
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2024-03-22 17:30:18 +0200

    Update main.cpp

--- Homework1/Playlist/main.cpp
+++ Homework1/Playlist/main.cpp
@@ -3,16 +3,58 @@
 #include <sstream>
 #include <cstring>
 #include <cmath>
-#pragma warning (disable : 4996)
+#include <cassert>
+//#pragma warning (disable : 4996)
 
 namespace Constants {
     constexpr size_t MAX_NAME_LEN = 64;
-    constexpr uint8_t GENRE_MAX = 1 + 2 + 4 + 8 + 16; // 31
-    constexpr short MAX_LYRICS_LEN = 256;
-    constexpr short MAX_SONGS_IN_PLAYLIST = 30;
+    constexpr size_t MAX_LYRICS_LEN = 256;
+    constexpr size_t MAX_SONGS_IN_PLAYLIST = 30;
+    constexpr char UNDEFINED_GENRE_STR[] = "Undefined";
+    constexpr char ROCK_GENRE_STR[] = "Rock";
+    constexpr char POP_GENRE_STR[] = "Pop";
+    constexpr char HIPHOP_GENRE_STR[] = "Hip-hop";
+    constexpr char ELECTRONIC_MUSIC_GENRE_STR[] = "Electronic music";
+    constexpr char JAZZ_GENRE_STR[] = "Jazz";
 }
 
-enum class Genre : uint8_t {
+struct Song {
+private:
+    int length = 0; //in seconds
+    size_t lyricsLen = 0;
+    uint8_t lyrics[Constants::MAX_LYRICS_LEN]{ 0 };
+    uint8_t genre = 0;
+    char name[Constants::MAX_NAME_LEN + 1] = "Unknown";
+
+    void printLengthConverted() const;
+    void printGenres() const;
+    void copyLyrics(const uint8_t* lyrics, size_t size);
+
+public:
+    Song() = default;
+    Song(const char* name, int lengthInSecs, const char* genre, const char* lyricsFile);
+
+    enum class Genre : uint8_t;
+
+    void setLength(int length);
+    void setLyrics(const char* lyrics);
+    void setLyricAt(int ind, uint8_t lyric);
+    void setName(const char* name);
+    void setGenre(const char* genre);
+
+    int getLength() const;
+    const char* getName() const;
+    uint8_t getGenre() const;
+    const uint8_t* getLyrics() const;
+    size_t getLyricsLen() const;
+
+    void printSong() const;
+    void printLyrics() const;
+    bool isValid() const;
+    void operator=(const Song& other);
+};
+
+enum class Song::Genre : uint8_t{
     UNDEFINED = 0,
     ROCK = 1,
     POP = 2,
@@ -42,93 +84,77 @@ int getFileSize(const char* file) {
 uint8_t getEnumFromChar(char ch) {
     switch (ch)
     {
-    case 'r': return (int)Genre::ROCK;
+    case 'r': return (int)Song::Genre::ROCK;
         break;
-    case 'p': return (int)Genre::POP;
+    case 'p': return (int)Song::Genre::POP;
         break;
-    case 'h': return (int)Genre::HIPHOP;
+    case 'h': return (int)Song::Genre::HIPHOP;
         break;
-    case 'e': return (int)Genre::ELECTRONIC_MUSIC;
+    case 'e': return (int)Song::Genre::ELECTRONIC_MUSIC;
         break;
-    case 'j': return (int)Genre::JAZZ;
+    case 'j': return (int)Song::Genre::JAZZ;
         break;
     default: return 0;
         break;
     }
 }
 
-struct Song {
-private:
-    int length = 0; //in seconds
-    short lyricsLen = 0;
-    uint8_t lyrics[Constants::MAX_LYRICS_LEN]{ 0 };
-    uint8_t genre = 0;
-    char name[Constants::MAX_NAME_LEN + 1] = "Unknown";
-    bool isValidSong = true;
+Song::Song(const char* name, int lengthInSecs, const char* genre, const char* lyricsFile) {
+    setLength(lengthInSecs);
+    setLyrics(lyricsFile);
+    setName(name);
+    setGenre(genre);
+}
 
-    void printLengthConverted() const;
-    void printGenres() const;
-
-public:
-    Song() = default;
-    Song(const char* name, int lengthInSecs, const char* genre, const char* lyricsFile) {
-        setLength(lengthInSecs);
-        setLyrics(lyricsFile);
-        setName(name);
-        setGenre(genre);
+void Song::setLength(int length) {
+    if (length > 0) {
+        this->length = length;
     }
-    void setLength(int length) {
-        if (length > 0) {
-            this->length = length;
-        }
-        else {
-            isValidSong = false;
-        }
-    }
-    void setLyrics(const char* lyrics) {
-        if (!lyrics) {
-            isValidSong = false;
-            return;
-        }
+}
 
-        int lyricsLen = getFileSize(lyrics); //binary stream o/c
-
+void Song::setLyrics(const char* lyrics) {
+    if (lyrics) {
+        int lyricsLen = getFileSize(lyrics);
         if (lyricsLen > Constants::MAX_LYRICS_LEN) {
-            isValidSong = false;
             return;
-        }
-
-        std::ifstream ifs(lyrics, std::ios::binary);
-        if (!ifs.is_open()) {
-            isValidSong = false;
-            return;
-        }
-        ifs.seekg(0);
-        this->lyricsLen = lyricsLen;
-
-        for (unsigned i = 0; i < lyricsLen; i++)
-        {
-            ifs.read((char*)&this->lyrics[i], sizeof(this->lyrics[i]));
-            //std::cout << lyrics[i] << std::endl;
-        }
-
-        ifs.close();
-    }
-    void setName(const char* name) {
-        if (!name || strlen(name) > Constants::MAX_NAME_LEN) {
-            isValidSong = false;
-            return;
-        }
-        else {
-            strcpy(this->name, name);
         }
     }
-    void setGenre(const char* genre) {
-        if (!genre) {
-            isValidSong = false;
-            return;
-        }
 
+    std::ifstream ifs(lyrics, std::ios::binary);
+    if (!ifs.is_open()) {
+        return;
+    }
+    ifs.seekg(0);
+    this->lyricsLen = lyricsLen;
+
+    for (unsigned i = 0; i < lyricsLen; i++)
+    {
+        ifs.read((char*)&this->lyrics[i], sizeof(this->lyrics[i]));
+    }
+
+    ifs.close();
+}
+
+void Song::setLyricAt(int ind, uint8_t lyric) {
+    if (ind < 0 || ind >= lyricsLen) {
+        return;
+    }
+    else {
+        lyrics[ind] = lyric;
+    }
+}
+
+void Song::setName(const char* name) {
+    if (!name || strlen(name) > Constants::MAX_NAME_LEN) {
+        return;
+    }
+    else {
+        strcpy_s(this->name, name);
+    }
+}
+
+void Song::setGenre(const char* genre) {
+    if (genre) {
         uint8_t result = 0;
         while (*genre)
         {
@@ -138,51 +164,46 @@ public:
 
         this->genre = result;
     }
-    int getLength() const {
-        return length;
-    }
-    const char* getName() const {
-        return name;
-    }
-    uint8_t getGenre() const {
-        return genre;
-    }
-    void addRhythm(int k);
-    void mixSong(const Song& other);
-    void printSong() const;
-    bool isValid() const;
-    void printLyrics() const;
-};
-
-void Song::addRhythm(int k) {
-    if (k < 0 || k > 8) {
-        return;
-    }
-
-    int powerOfTwo = k - 1;
-
-    for (int i = lyricsLen; i >= 0; i--)
-    {
-        while (powerOfTwo < 8)
-        {
-            int toAdd = (1 << powerOfTwo);
-            lyrics[i] += toAdd;
-            powerOfTwo += k;
-        }
-
-        powerOfTwo -= 8;
-    }
 }
 
-void Song::mixSong(const Song& other) {
-    if (!isValidSong || !other.isValidSong) {
+int Song::getLength() const {
+    return length;
+}
+
+const char* Song::getName() const {
+    return name;
+}
+
+uint8_t Song::getGenre() const {
+    return genre;
+}
+
+const uint8_t* Song::getLyrics() const {
+    return lyrics;
+}
+
+size_t Song::getLyricsLen() const {
+    return lyricsLen;
+}
+
+void Song::copyLyrics(const uint8_t* lyrics, size_t size) {
+    if (!lyrics) {
         return;
     }
+    else {
+        for (unsigned i = 0; i < size; i++)
+        {
+            this->lyrics[i] = lyrics[i];
+        }
 
-    unsigned short bytesToMix = std::min(lyricsLen, other.lyricsLen);
+        if (this->getLyricsLen() > size) {
+            for (unsigned i = this->getLyricsLen(); i < size; i++)
+            {
+                this->lyrics[i] = 0;
+            }
+        }
 
-    for (unsigned i = 0; i < bytesToMix; i++) {
-        lyrics[i] = (lyrics[i] ^ other.lyrics[i]);
+        this->lyricsLen = std::max(this->getLyricsLen(), size);
     }
 }
 
@@ -224,47 +245,43 @@ void Song::printGenres() const {
     bool printSep = false;
 
     if (genre == 0) {
-        std::cout << "Undefied";
+        std::cout << Constants::UNDEFINED_GENRE_STR;
         return;
     }
     if ((genre & (uint8_t)Genre::ROCK) != 0) {
-        std::cout << "Rock";
+        std::cout << Constants::ROCK_GENRE_STR;
         printSep = true;
     }
     if ((genre & (uint8_t)Genre::POP) != 0) {
         if (printSep) {
             std::cout << sep;
         }
-        std::cout << "Pop";
+        std::cout << Constants::POP_GENRE_STR;
         printSep = true;
     }
     if ((genre & (uint8_t)Genre::HIPHOP) != 0) {
         if (printSep) {
             std::cout << sep;
         }
-        std::cout << "Hip-hop";
+        std::cout << Constants::HIPHOP_GENRE_STR;
         printSep = true;
     }
     if ((genre & (uint8_t)Genre::ELECTRONIC_MUSIC) != 0) {
         if (printSep) {
             std::cout << sep;
         }
-        std::cout << "Electronic music";
+        std::cout << Constants::ELECTRONIC_MUSIC_GENRE_STR;
         printSep = true;
     }
     if ((genre & (uint8_t)Genre::JAZZ) != 0) {
         if (printSep) {
             std::cout << sep;
         }
-        std::cout << "Jazz";
+        std::cout << Constants::JAZZ_GENRE_STR;
     }
 }
 
 void Song::printSong() const {
-    if (!isValidSong) {
-        return;
-    }
-
     std::cout << "Name: " << name << ", ";
     std::cout << "Length: ";
     printLengthConverted();
@@ -274,31 +291,63 @@ void Song::printSong() const {
     std::cout << std::endl;
 }
 
-bool Song::isValid() const {
-    return isValidSong;
-}
-
 void Song::printLyrics() const {
     for (unsigned i = 0; i < lyricsLen; i++)
     {
-        std::cout << (char)lyrics[i] << " ";
+        std::cout << '\'' << lyrics[i] << "\' ";
     }
+    std::cout << std::endl;
+}
+
+void Song::operator=(const Song & other) {
+    this->setName(other.getName());
+    this->genre = other.getGenre();
+    this->setLength(other.getLength());
+    copyLyrics(other.getLyrics(), other.getLyricsLen());
 }
 
 class Playlist {
 private:
+    
     Song songs[Constants::MAX_SONGS_IN_PLAYLIST];
-    unsigned short songsCount = 0;
+    size_t songsCount = 0;
+
+    int getSongIndex(const char* name) const;
+    void sort(bool(*pred)(Song, Song));
 
 public:
     Playlist() = default;
     void addSong(const char* name, int length, const char* genre, const char* lyricsFile);
     void printSongs() const;
-    Song getSongByName(const char* name) const;
-    void printAllSongsWithGenre(char genre) const;
-    void sort(bool(*pred)(Song, Song));
-    void saveSongToFile(const char* songName, const char* fileName) const;
+    void findName (const char* name) const;
+    void findGenre(char genre) const;
+    void saveSong(const char* song, const char* file) const;
+    enum class SortingOptions;
+    void sort(SortingOptions sortType);
+    void mix(const char* song, int k);              //Add rhythm to song
+    void mix(const char* song1, const char* song2); //Mix 2 songs
 };
+
+enum class Playlist::SortingOptions{
+    OTHER,
+    NAME,
+    LENGTH
+};
+
+int Playlist::getSongIndex(const char* name) const {
+    if (!name) {
+        return -1;
+    }
+
+    for (unsigned i = 0; i < songsCount; i++)
+    {
+        if (strcmp(songs[i].getName(), name) == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
 
 void Playlist::addSong(const char* name, int lengthInSecs, const char* genre, const char* lyricsFile) {
     if (!lyricsFile || !genre) {
@@ -306,38 +355,28 @@ void Playlist::addSong(const char* name, int lengthInSecs, const char* genre, co
     }
 
     if (songsCount < Constants::MAX_SONGS_IN_PLAYLIST) {
-        songs[songsCount].setName(name);
-        songs[songsCount].setLength(lengthInSecs);
-        songs[songsCount].setGenre(genre);
-        songs[songsCount].setLyrics(lyricsFile);
+        Song s(name, lengthInSecs, genre, lyricsFile);
+        songs[songsCount] = s;
         songsCount++;
     }
 }
 
 void Playlist::printSongs() const {
+    std::cout << "------------------------------------------------\n\n";
     for (unsigned i = 0; i < songsCount; i++)
     {
         songs[i].printSong();
         //songs[i].printLyrics();
         std::cout << std::endl;
     }
+    std::cout << "------------------------------------------------\n\n";
 }
 
-Song Playlist::getSongByName(const char* name) const {
-    if (!name) {
-        Song s;
-        return s;
-    }
-
-    for (unsigned i = 0; i < songsCount; i++)
-    {
-        if (strcmp(songs[i].getName(), name) == 0) {
-            return songs[i];
-        }
-    }
+void Playlist::findName(const char* name) const {
+    songs[getSongIndex(name)].printSong();
 }
 
-void Playlist::printAllSongsWithGenre(char genre) const {
+void Playlist::findGenre(char genre) const {
     uint8_t genreNum = getEnumFromChar(genre);
 
     for (unsigned i = 0; i < songsCount; i++)
@@ -364,45 +403,92 @@ void Playlist::sort(bool(*pred)(Song, Song)) {
     }
 }
 
-void Playlist::saveSongToFile(const char* songName, const char* fileName) const {
-    if (!songName || !fileName) {
+void Playlist::sort(Playlist::SortingOptions sortType) {
+    if (sortType == SortingOptions::NAME) {
+        sort([](Song a, Song b) {return strcmp(a.getName(), b.getName()) < 0; }); //sort by name
+    }
+    else if (sortType == SortingOptions::LENGTH) {
+        sort([](Song a, Song b) {return a.getLength() < b.getLength(); }); //sort by length
+    }
+    else {
+        std::cout << "Cannot sort playlist.\n"; 
+        return;
+    }
+}
+
+void Playlist::saveSong(const char* song, const char* file) const {
+    if (!song || !file) {
         return;
     }
 
-    std::ofstream ofs(fileName, std::ios::binary | std::ios::trunc);
+    int index = getSongIndex(song);
+    Song s = songs[index];
+
+    std::ofstream ofs(file, std::ios::binary);
     if (!ofs.is_open()) {
+        std::cout << "Cannot open file for writing.\n";
         return;
     }
 
-    Song s = getSongByName(songName);
+    ofs.write((const char*)s.getLyrics(), s.getLyricsLen() * sizeof(s.getLyrics()[0]));
+}
 
-    ofs.write((const char*)&s, sizeof(s));
-    ofs.close();
+void Playlist::mix(const char* song, int addedRhythm) {
+    if (addedRhythm < 0 || addedRhythm > 8 || !song) {
+        return;
+    }
+
+    int index = getSongIndex(song);
+    Song result = songs[index];
+    unsigned powerOfTwo = addedRhythm - 1;
+
+    for (int i = result.getLyricsLen() - 1; i >= 0; i--)
+    {
+        uint8_t newLyric = result.getLyrics()[i];
+
+        while (powerOfTwo < 8)
+        {
+            int toAdd = (1 << powerOfTwo);
+
+            newLyric = (newLyric | toAdd);
+            result.setLyricAt(i, newLyric);
+
+            powerOfTwo += addedRhythm;
+        }
+        powerOfTwo -= 8;
+    }
+
+    unsigned resultInd = getSongIndex(result.getName());
+
+    songs[resultInd] = result;
+
+    result.printLyrics();
+}
+
+void Playlist::mix(const char* song1, const char* song2) {
+    if (!song1 || !song2) {
+        return;
+    }
+
+    int resultInd = getSongIndex(song1);
+    Song result = songs[resultInd];
+
+    int otherInd = getSongIndex(song2);
+    Song other = songs[otherInd];
+
+    size_t lyricsToMix = std::min(result.getLyricsLen(), other.getLyricsLen());
+
+    for (unsigned i = 0; i < lyricsToMix; i++)
+    {
+        uint8_t newLyric = (result.getLyrics()[i] ^ other.getLyrics()[i]);
+
+        result.setLyricAt(i, newLyric);
+    }
+
+    songs[resultInd] = result;
 }
 
 int main()
 {
-    // !!!!!!!!!!!! Reading the lyrics from a file means reading the whole song and then copying the lyrics (ig?)
-    uint8_t lyrics[3];
-    lyrics[0] = 67; // 0 1 0 0  0 0 1 1
-    lyrics[1] = 111;// 0 1 1 0  1 1 1 1
-    lyrics[2] = 119;// 0 1 1 1  0 1 1 1
-    const char file[] = "song1.dat";
-    std::ofstream ofs(file, std::ios::binary);
-    ofs.write((const char*)lyrics, sizeof(lyrics));
-    ofs.close();
 
-    Playlist p;
-    p.addSong("Pesen", 413, "jrp", file);
-    p.addSong("103 kila", 146, "rh", file);
-    p.addSong("tiktok short", 79, "e", file);
-    p.addSong("rech na stalin", 314, "je", file);
-
-    p.sort( [](Song a, Song b) {return a.getLength() < b.getLength(); }); //sort by length
-    p.printSongs();
-    std::cout << "-----------------------\n\n";
-    p.sort([](Song a, Song b) {return strcmp(a.getName(), b.getName()) < 0; }); //sort by name
-    p.printSongs();
-
-    p.saveSongToFile("103 kila", "newFile.dat");
 }
