d22a2767c3e2c0e9863c3280fda80be0e53ecd3a
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2024-04-16 23:28:46 +0300

    Create Exceptions.h

---
+++ Homework2/MultiSet/Exceptions.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Exceptions {
+	constexpr char INVALID_MAX_NUM_EXCEPTION[] = "Error! Multiset must have a positive maximum value!";
+	constexpr char INVALID_BIT_STORAGE_EXCEPTION[] = "Error! Multiset must use between 1 and 8 bits for storing infortmation!";
+	constexpr char BAD_ALLOC_EXCEPTION[] = "Error! Memory cannot be allocated for storage!";
+	constexpr char INVALID_NEW_NUMBER_EXCEPTION[] = "Error! Cannot add number outside the allowed range!";
+	constexpr char INVALID_FILE_NAME_EXCEPTION[] = "Error! Cannot read file name!";
+	constexpr char CLOSED_FILE_EXCEPTION[] = "Error! Cannot open file for reading/writing!";
+}
