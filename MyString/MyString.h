e51e7cc5281977a61919db974c7f181c8a730e5c
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2024-05-23 17:51:49 +0300

    Update MyString.h

--- MyString/MyString.h
+++ MyString/MyString.h
@@ -1,23 +1,49 @@
 #pragma once
+#define st size_t
 
 class MyString {
 private:
 	char* text = nullptr;
-	size_t size = 0;
+	st size = 0;
+	st capacity = 0;
 
 	void copyFrom(const MyString&);
+	void moveFrom(MyString&&);
 	void free();
+
+	void resize();
+	void resize(st);
 
 public:
 	MyString();
-	MyString(const char* str);
-	MyString(const MyString& other);
+	MyString(st);
+	MyString(const char*);
+	MyString(const MyString&); // C.C.
+	MyString(MyString&&) noexcept; // M.C.
 
-	size_t length() const;
+	st getLength() const;
 
-	MyString& operator=(const MyString& other);
-	bool operator==(const MyString& other) const;
-	//friend std::ostream& operator<<(std::ostream&, const MyString&) const;
+	const char* c_str() const;
+
+	char& operator[](int);
+	const char& operator[](int) const;
+
+	MyString& operator=(const MyString&); // OP=
+	MyString& operator=(MyString&&) noexcept; // Move OP=
+
+	friend std::ostream& operator<<(std::ostream&, const MyString&);
+	friend std::istream& operator>>(std::istream&, MyString&);
+
+	MyString& operator+=(const MyString&);
 
 	~MyString();
 };
+
+MyString operator+(const MyString&, const MyString&);
+
+bool operator==(const MyString&, const MyString&);
+bool operator!=(const MyString&, const MyString&);
+bool operator<(const MyString&, const MyString&);
+bool operator<=(const MyString&, const MyString&);
+bool operator>(const MyString&, const MyString&);
+bool operator>=(const MyString&, const MyString&);
