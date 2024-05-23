b63df2dc3b5cbb623342bd45c7fd7fb5f4636d1b
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2024-05-23 17:52:07 +0300

    Update MyString.cpp

--- MyString/MyString.cpp
+++ MyString/MyString.cpp
@@ -5,12 +5,19 @@
 
 MyString::MyString() = default;
 
+MyString::MyString(st cap)
+{
+	text = new char[cap + 1];
+	capacity = cap;
+	text[0] = '\0';
+}
+
 MyString::MyString(const char* str) {
 	if (!str) {
 		return;
 	}
 
-	size = strlen(str);
+	size = capacity = strlen(str);
 	text = new char[size + 1];
 	strcpy(text, str);
 }
@@ -19,13 +26,59 @@ MyString::MyString(const MyString& other) {
 	copyFrom(other);
 }
 
-size_t MyString::length() const {
+MyString::MyString(MyString&& other) noexcept
+{
+	moveFrom(std::move(other));
+}
+
+st MyString::getLength() const {
 	return size;
 }
 
+const char* MyString::c_str() const
+{
+	return text;
+}
+
+char& MyString::operator[](int ind)
+{
+	return text[ind];
+}
+
+const char& MyString::operator[](int ind) const
+{
+	return text[ind];
+}
+
 MyString& MyString::operator=(const MyString& other) {
-	free();
-	copyFrom(other);
+	if (this != &other) {
+		if (other.size > capacity) {
+			free();
+			copyFrom(other);
+		}
+	}
+
+	return *this;
+}
+
+MyString& MyString::operator=(MyString&& other) noexcept
+{
+	if (this != &other) {
+		free();
+		moveFrom(std::move(other));
+	}
+
+	return *this;
+}
+
+MyString& MyString::operator+=(const MyString& other)
+{
+	if (capacity < size + other.size) {
+		resize(size + other.size);
+	}
+
+	strcat(text, other.text);
+	size += other.size;
 
 	return *this;
 }
@@ -37,7 +90,21 @@ MyString::~MyString() {
 void MyString::free() {
 	delete[] text;
 	text = nullptr;
-	size = 0;
+	size = capacity = 0;
+}
+
+void MyString::resize()
+{
+	resize(capacity * 2);
+}
+
+void MyString::resize(st newCap)
+{
+	char* newData = new char[newCap + 1];
+	strcpy(newData, text);
+	delete[] text;
+	text = newData;
+	capacity = newCap;
 }
 
 void MyString::copyFrom(const MyString& other) {
@@ -46,27 +113,78 @@ void MyString::copyFrom(const MyString& other) {
 			return;
 		}
 		size = other.size;
+		capacity = other.capacity;
 		text = new char[size + 1];
 		strcpy(text, other.text);
 	}
 }
 
-bool MyString::operator==(const MyString& other) const {
-	if (!text && !other.text) {
-		return true;
-	}
-	else if (!text || !other.text) {
-		return false;
-	}
-	else { 
-		return !strcmp(text, other.text);
-	}
+void MyString::moveFrom(MyString&& other)
+{
+	text = other.text;
+	size = other.size;
+	capacity = other.capacity;
+	other.text = nullptr;
+	other.size = other.capacity = 0;
 }
 
-//friend std::ostream& MyString::operator<<(std::ostream& os, const MyString& myStr) const {
-//	if (!myStr.text) {
-//		return os;
-//	}
-//
-//	return os << myStr.text;
-//}
+MyString operator+(const MyString& first, const MyString& second)
+{
+	MyString result(first.getLength() + second.getLength());
+	result += first;
+	result += second;
+	return result;
+}
+
+bool operator==(const MyString& first, const MyString& second) {
+	return strcmp(first.c_str(), second.c_str()) == 0;
+}
+
+bool operator!=(const MyString& first, const MyString& second)
+{
+	return strcmp(first.c_str(), second.c_str()) != 0;
+}
+
+bool operator<(const MyString& first, const MyString& second)
+{
+	return strcmp(first.c_str(), second.c_str()) < 0;
+}
+
+bool operator<=(const MyString& first, const MyString& second)
+{
+	return strcmp(first.c_str(), second.c_str()) <= 0;
+}
+
+bool operator>(const MyString& first, const MyString& second)
+{
+	return strcmp(first.c_str(), second.c_str()) > 0;
+}
+
+bool operator>=(const MyString& first, const MyString& second)
+{
+	return strcmp(first.c_str(), second.c_str()) >= 0;
+}
+
+std::ostream& operator<<(std::ostream& os, const MyString& myStr) {
+	if (!myStr.text) {
+		return os;
+	}
+
+	return os << myStr.text;
+}
+
+std::istream& operator>>(std::istream& is, MyString& myStr)
+{
+	char buff[1024];
+	is >> buff;
+	st buffSize = strlen(buff);
+
+	if (buffSize > myStr.capacity) {
+		myStr.resize(buffSize);
+	}
+
+	strcpy(myStr.text, buff);
+	myStr.size = buffSize;
+
+	return is;
+}
